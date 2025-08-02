f576ebe5a398a22b8dbd03bd6e6144be93378f42
gaabiceranto <gabiceranto@gmail.com>
2025-08-02 15:58:28 -0300

    feat: setup lazy-loaded feature modules with sidenav navigation

--- projects/frontend/src/app/app.config.ts
+++ projects/frontend/src/app/app.config.ts
@@ -1,12 +1,10 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
-  ]
+  providers: [provideRouter(routes), MatToolbarModule, MatSidenavModule, MatListModule]
 };
