3f5a1390c1e1e3f84cd6767e07679d3a52172e3d
gaabiceranto <gabiceranto@gmail.com>
2025-08-03 18:13:07 -0300

    feat: create DevForm component with user input fields and styling

---
+++ projects/frontend/src/app/components/dev-form/dev-form.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+
+@Component({
+  selector: 'app-dev-form',
+  standalone: true,
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDividerModule,
+  ],
+  templateUrl: './dev-form.component.html',
+  styleUrls: ['./dev-form.component.scss'],
+})
+
+export class DevFormComponent {}
