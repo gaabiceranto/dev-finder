6853e8d5b7d5daf7cef8c22952250aa7e26f9411
gaabiceranto <gabiceranto@gmail.com>
2025-08-03 13:54:07 -0300

    refactor: remove unused toolbar and styles from app.html, update HomeComponent import in app.routes.ts

--- projects/frontend/src/app/app.routes.ts
+++ projects/frontend/src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './features/cadastro/home.component';
+import { HomeComponent } from './features/home/home.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
