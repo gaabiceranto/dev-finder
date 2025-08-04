f8513ee40cbbf57f4260399589d05a04e747fff3
gaabiceranto <gabiceranto@gmail.com>
2025-08-03 22:35:46 -0300

    feat: add email icon to dev card

--- projects/frontend/src/app/components/dev-card/dev-card.component.ts
+++ projects/frontend/src/app/components/dev-card/dev-card.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
 import { Developer } from '../../models/developer.model';
 
 @Component({
   selector: 'app-dev-card',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatIconModule],
   templateUrl: './dev-card.component.html',
   styleUrls: ['./dev-card.component.scss'],
 })
