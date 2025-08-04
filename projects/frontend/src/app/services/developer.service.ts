af84eda5d9d78196c871c18803a5a6dc1096551e
gaabiceranto <gabiceranto@gmail.com>
2025-08-04 10:57:10 -0300

    fix: resolve loading state issue after developer deletion

--- projects/frontend/src/app/services/developer.service.ts
+++ projects/frontend/src/app/services/developer.service.ts
@@ -1,15 +1,19 @@
 import { Injectable } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { Developer } from '../models/developer.model';
 import * as DevActions from '../store/dev/dev.actions';
 import { DevState } from '../store/dev/dev.state';
+import { LocalStorageService } from './local-storage.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DeveloperService {
-  constructor(private store: Store<{ dev: DevState }>) {}
+  constructor(
+    private store: Store<{ dev: DevState }>,
+    private localStorageService: LocalStorageService
+  ) {}
 
   getDevelopers(): Observable<Developer[]> {
     return this.store.select((state) => state.dev.developers);
@@ -25,26 +29,26 @@ export class DeveloperService {
 
   addDeveloper(developer: Developer): void {
     this.store.dispatch(DevActions.addDev(developer));
+    this.localStorageService.addDeveloper(developer);
     this.store.dispatch(DevActions.addDevSuccess(developer));
   }
 
   loadDevelopers(): void {
     this.store.dispatch(DevActions.loadDevs());
+    const developers = this.localStorageService.getDevelopers();
+    this.store.dispatch(DevActions.loadDevsSuccess(developers));
   }
 
   deleteDeveloper(id: string): Observable<void> {
     this.store.dispatch(DevActions.deleteDev(id));
-    return new Observable((observer) => {
-      setTimeout(() => {
-        this.store.dispatch(DevActions.deleteDevSuccess(id));
-        observer.next();
-        observer.complete();
-      }, 100);
-    });
+    this.localStorageService.deleteDeveloper(id);
+    this.store.dispatch(DevActions.deleteDevSuccess(id));
+    return of(void 0);
   }
 
   updateDeveloper(developer: Developer): void {
     this.store.dispatch(DevActions.updateDev(developer));
+    this.localStorageService.updateDeveloper(developer);
     this.store.dispatch(DevActions.updateDevSuccess(developer));
   }
 }
