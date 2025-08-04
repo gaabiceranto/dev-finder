82e856569e40a3b69cadec4dbf1f0b89aa062c11
gaabiceranto <gabiceranto@gmail.com>
2025-08-04 12:53:16 -0300

    fix: update Firebase configuration

--- projects/frontend/src/app/app.config.ts
+++ projects/frontend/src/app/app.config.ts
@@ -9,12 +9,12 @@ import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 
 const firebaseConfig = {
-  apiKey: '',
-  authDomain: '',
-  projectId: '',
-  storageBucket: '',
-  messagingSenderId: '',
-  appId: '',
+  apiKey: 'AIzaSyCpypkHgtG3PVSd4cIS2V5sb8TgMq_vkjc',
+  authDomain: 'dev-finder-8a8e0.firebaseapp.com',
+  projectId: 'dev-finder-8a8e0',
+  storageBucket: 'dev-finder-8a8e0.firebasestorage.app',
+  messagingSenderId: '335621662699',
+  appId: '1:335621662699:web:69a890d15011440119dbae',
 };
 
 export const appConfig: ApplicationConfig = {
